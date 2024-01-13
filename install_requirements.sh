e7a9bd1a05f97b057f0666b5613d112213e3ba39
MaksWisniewski <makswisniewski@gmail.com>
2024-01-13 21:27:52 +0100

    added note for req.txt

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,6 @@
 # Create new requirements.txt
 # pip freeze > requirements.txt
 
-
+# Mozliwe ze trzeba:
+#   pip install virtualenv
 virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
