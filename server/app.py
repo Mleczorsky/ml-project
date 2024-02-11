7976896d315943b34c28882345911f65d1ff15dc
Mleczorsky <myster.pluszak@gmail.com>
2024-02-12 00:20:35 +0100

    Formatting on frontend changed

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    list_of_models = [f'<li> {model} <li>' for model in models]
+    list_of_models = "".join([f'<li> {model} <li>' for model in models])
     return f'<h1>Hello from Flask & Docker</h1><ul>{list_of_models}</ul>'
 
 
