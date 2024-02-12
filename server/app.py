4b77e31a4630b9abc11ccfd31125f25dcbeb2b3b
Mleczorsky <myster.pluszak@gmail.com>
2024-02-12 01:24:58 +0100

    Some changes were done for real. Shit kinda working no cap

--- server/app.py
+++ server/app.py
@@ -1,13 +1,36 @@
-from flask import Flask
-from models import models
+from flask import Flask, render_template
+# from models import models
+models = ['mock']
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
+def root():
     list_of_models = "".join([f'<li> {model} <li>' for model in models])
-    return f'<h1>Hello from Flask & Docker</h1><ul>{list_of_models}</ul>'
+    href = "<a href='./photo'> PHOTO </a>"
+    return f'<h1>Hello from Flask & Docker</h1>{href}<ul>{list_of_models}</ul>'
 
+@app.route('/photo')
+def photo():
+    return render_template('photo.html')
+
+
+@app.route('/processing', methods=['POST'])
+def process():
+    file = request.files['image']
+
+    img = Image.open(file.stream)
+
+    data = file.stream.read()
+    # data = base64.encodebytes(data)
+    data = base64.b64encode(data).decode()
+
+    return jsonify({
+        'msg': 'success',
+        'size': [img.width, img.height],
+        'format': img.format,
+        'img': data
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ server/extract_features.py
@@ -0,0 +1,47 @@
+import numpy as np
+import tensorflow.keras.applications.vgg16 as vgg16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import Model
+import os
+
+
+# Load the VGG16 model
+model = vgg16.VGG16()
+
+# remove the prediction layer
+model = Model(model.inputs, model.layers[-2].output)
+
+
+def extract(directory = None, images = None):
+    '''
+    `directory` must end with /
+
+    `images` - np.array of images
+    '''
+    features = []
+
+    if directory:
+        for image_file in os.listdir(directory):
+            image_path = os.path.join(directory, image_file)
+            img = img_to_array(load_img(image_path, target_size=(224, 224)))
+
+            # reshape the image for the model
+            img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+            # preprocess the image
+            pp = preprocess_input(img)
+            # extract features
+            features.append(model.predict(pp, verbose=0))
+
+    elif not images is None:
+        for img in images:
+            img = img_to_array(img.resize((224, 224)))
+
+            # reshape the image for the model
+            img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+            # preprocess the image
+            pp = preprocess_input(img)
+            # extract features
+            features.append(model.predict(pp, verbose=0))
+
+    return np.array(features).reshape(-1, 4096)
